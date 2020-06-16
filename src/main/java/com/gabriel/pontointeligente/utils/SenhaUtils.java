a995b2fee547169bdbef50f6ee7e77833ae3e7c6
gabriel luz <gabriel_luz_94@hotmail.com>
2020-06-15 23:38:59 -0300

    Implementacao BCrypt e flyway

---
+++ src/main/java/com/gabriel/pontointeligente/utils/SenhaUtils.java
@@ -0,0 +1,37 @@
+package com.gabriel.pontointeligente.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class SenhaUtils {
+	/**
+	 * Gera um hash utilizando o BCrypt.
+	 *
+	 * @param senha
+	 * @return String
+	 */
+	public static String gerarBCrypt(String senha) {
+		if (senha == null) {
+			return senha;
+		}
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		return bCryptEncoder.encode(senha);
+	}
+
+	/**
+	 * Verifica se a senha é válida.
+	 *
+	 * @param senha
+	 * @param senhaEncoded
+	 * @return boolean
+	 */
+	public static boolean senhaValida(String senha, String senhaEncoded) {
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		return bCryptEncoder.matches(senha, senhaEncoded);
+	}
+	
+	/* Gerar encode para senha */
+	// String senhaEncoded = SenhaUtils.gerarBCrypt( "123456");
+	/* Validar senha */
+	// boolean senhaValida = SenhaUtils.senhaValida( "123456", senhaEncoded);
+	
+}
